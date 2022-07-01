307929d5e7667702ef9862826f94cae3639ce11e
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2022-07-01 15:10:29 +0300

    Add files via upload

---
+++ HomeWork2/tast1.py
@@ -0,0 +1,29 @@
+from itertools import *
+
+
+def way_length(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+
+
+way = [(0, 2), (2, 5), (5, 2), (6, 6), (8, 3), (0, 2)]  # изначальный путь, если не измениться, то и минимальный значит
+min_length = 10**10
+for i in permutations(way[1:-1]):
+    path = [(0, 2)]
+    path.extend(i)
+    path.append((0, 2))
+    # print(path)
+    res = 0
+    for k in range(1, len(path)):
+        res += way_length(path[k], path[k-1])
+    if res < min_length:
+        way = path.copy()
+        min_length = res
+
+# [(0, 2), (5, 2), (8, 3), (6, 6), (2, 5), (0, 2)]  # Второй кратчайший путь (в обратном порядке точек)
+
+res = 0
+print(way[0], end=' ')
+for i in range(1, len(way)):
+    res += way_length(way[i], way[i-1])
+    print('->', way[i], [res], end=' ')
+print('=', min_length)
